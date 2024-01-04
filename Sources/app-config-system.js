8f7ce2d4f95e99743f1c33ec803dc0ae0d59476c
Sebastien Jodogne <s.jodogne@gmail.com>
2024-01-04 15:20:42 +0100

    update year to 2024

--- Sources/app-config-system.js
+++ Sources/app-config-system.js
@@ -1,6 +1,6 @@
 /**
- * SPDX-FileCopyrightText: 2023 Sebastien Jodogne, UCLouvain, Belgium,
- * and 2018-2023 Open Health Imaging Foundation
+ * SPDX-FileCopyrightText: 2023-2024 Sebastien Jodogne, UCLouvain, Belgium,
+ * and 2018-2024 Open Health Imaging Foundation
  * SPDX-License-Identifier: MIT
  */
 
